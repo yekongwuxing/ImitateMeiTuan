557a01c53b34ed12b7f53b1bcdb97ce6702477b1
jiangmm <jiangmm@jchvip.com>
2018-08-15 16:21:47 +0800

    附近导航栏左右按钮

--- src/pages/Nearby/NearbyPage.js
+++ src/pages/Nearby/NearbyPage.js
@@ -1,9 +1,39 @@
 import React,{Component} from 'react';
-import {View} from 'react-native';
+import {
+    View,
+    TouchableOpacity,
+    Image,
+    Text,
+    StyleSheet
+} from 'react-native';
+
+import NavigationItem from '../../widget/NavigationItem';
+import Color from "../../widget/Color";
+import screen from "../../common/screen";
+import {Paragraph} from "../../widget/Text";
 
 export  default class NearbyPage extends Component{
     static navigationOptions = ({navigation}:any) => ({
-        headerTitle:'附近'
+        headerLeft:(
+            <TouchableOpacity style={styles.leftContainer}>
+                <Image source={require('../../images/public/icon_food_merchant_address.png')}
+                       style={{width:13,height:16,margin:5}}
+                />
+                <Text style={{fontSize:17,color:'#333333'}}>福州 鼓楼</Text>
+            </TouchableOpacity>
+
+        ),
+        headerRight:(
+            <TouchableOpacity style={styles.searchBar}>
+                <Image source={require('../../images/home/search_icon.png')}
+                       style={styles.searchIcon}
+                />
+                <Paragraph>找附近吃喝玩乐</Paragraph>
+
+            </TouchableOpacity>
+        ),
+        headerStyle: {backgroundColor: 'white'},
+
     })
     render(){
         return(
@@ -12,3 +42,31 @@ export  default class NearbyPage extends Component{
     }
 
 }
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:Color.paper
+    },
+    leftContainer:{
+        flexDirection:'row',
+        justifyContent:'center',
+        alignItems:'center',
+        padding:10
+
+    },
+    searchBar:{
+        width:screen.width * 0.65,
+        height:30,
+        borderRadius:20,
+        flexDirection:'row',
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'#eeeeee',
+        marginRight:20
+    },
+    searchIcon:{
+        width:20,
+        height:20,
+        margin:5
+    }
+})
