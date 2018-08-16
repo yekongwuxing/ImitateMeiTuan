600b2cb031884191312066aef1090bac8490485f
jiangmm <jiangmm@jchvip.com>
2018-08-16 10:32:58 +0800

    react-native-scrollable-tab-view标签切换

--- src/pages/Nearby/NearbyPage.js
+++ src/pages/Nearby/NearbyPage.js
@@ -11,6 +11,7 @@ import NavigationItem from '../../widget/NavigationItem';
 import Color from "../../widget/Color";
 import screen from "../../common/screen";
 import {Paragraph} from "../../widget/Text";
+import ScrollableTabView,{DefaultTabBar,ScrollableTabBar} from 'react-native-scrollable-tab-view'
 
 export  default class NearbyPage extends Component{
     static navigationOptions = ({navigation}:any) => ({
@@ -36,8 +37,28 @@ export  default class NearbyPage extends Component{
 
     })
     render(){
+        let titles = ['享美食','住酒店','爱玩乐','全部']
+        let types = [
+            ['热门', '面包甜点', '小吃快餐', '川菜', '日本料理', '韩国料理', '台湾菜', '东北菜'],
+            ['热门', '商务出行', '公寓民宿', '情侣专享', '高星特惠'],
+            ['热门', 'KTV', '足疗按摩', '洗浴汗蒸',  '电影院', '美发', '美甲'],
+            []
+        ]
         return(
-            <View style={{flex:1}}></View>
+            <ScrollableTabView style={styles.container}
+                               renderTabBar={() => <DefaultTabBar />}
+                               tabBarUnderlineStyle={styles.tabBarUnderline}
+                               tabBarActiveTextColor='#fe566d'
+                               tabBarInactiveTextColor='#555555'
+                               tabBarBackgroundColor='white'
+            >
+                <Text style={styles.textStyle} tabLabel='tab1'>tab1</Text>
+                <Text style={styles.textStyle} tabLabel='tab2'>tab2</Text>
+                <Text style={styles.textStyle} tabLabel='tab3'>tab3</Text>
+                <Text style={styles.textStyle} tabLabel='tab4'>tab4</Text>
+
+
+            </ScrollableTabView>
         );
     }
 
@@ -68,5 +89,16 @@ const styles = StyleSheet.create({
         width:20,
         height:20,
         margin:5
+    },
+    tabBarUnderline:{
+        backgroundColor:'#fe566d',
+        width:screen.width/4,
+        height:screen.onePixel
+
+    },
+    textStyle: {
+        flex: 1,
+        fontSize:20,
+        textAlign:'center',
     }
 })
